8b7f4c223e94395f1440529fda9fdb5908ba0952
leugiim <miguelrl.92@gmail.com>
2023-04-19 21:59:09 +0200

    Refactor infrastructure files

--- src/Users/Infrastructure/UserFirebaseRepository.ts
+++ src/Users/Infrastructure/UserFirebaseRepository.ts
@@ -1,5 +1,6 @@
 import { User } from "../Domain/User";
 import { UserRepository } from "../Domain/UserRepository";
+import { firebase } from "../../_Shared/Infrastructure/Firebase";
 
 export class UserFirebaseRepository implements UserRepository {
   async findById(id: string): Promise<User | null> {
