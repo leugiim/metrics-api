8a28c05d2858caa099956e7511c8300e590460e8
leugiim <miguelrl.92@gmail.com>
2023-04-21 20:34:17 +0200

    Add coverage and edit tests

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,6 @@
 module.exports = {
   preset: "ts-jest",
   testEnvironment: "node",
-  setupFilesAfterEnv: ["./tests/setup.ts"],
   moduleNameMapper: {
     "^@/(.*)$": "<rootDir>/src/$1",
   },
--- tests/version.test.ts
+++
@@ -1,8 +0,0 @@
-import { request } from "./setup";
-
-describe("Test the version path", () => {
-  test("It should response the GET method", async () => {
-    const response = await request.get("/version");
-    expect(response.status).toBe(200);
-  });
-});
